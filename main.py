9261755447cd0b19aa89195a77126b0f7180b7cf
Johan Ullen <johanullen@hotmail.com>
2020-04-03 02:32:44 +0200

    greedy working!

--- main.py
+++ main.py
@@ -25,56 +25,59 @@ def brute_force(s, e, rand="all"):
     print("*" * 40)
 
 
-def greedy(s, e, rand="all"):
+def greedy(s, e, initial="all"):
     start = time()
     print(f"starting exp={e} with s={s}")
-    a = A(s, e, rand)
-    smallest = diff = a.diff()
-    sign = utils.intsign(diff)
-    pos = int(math.log(sign * diff, a.e)) if diff else None
+    a = A(s, e, initial)
+    smallest = a.diff()
 
     history = {a.val}
     print(history)
     i = 0
     while smallest != 0:
+        # print("*"*40)
         try:
-            # print(i, f"|| {s**e}-{a.val}={diff} => pos={pos} ||", a)
-            sign = utils.intsign(diff)
-            pos = int(math.log(sign * diff, a.e))
-
-            # need to check wether pos it set
+            pos = a.get_most_impactful_bit()
             pos_sign = -1 if a.is_set(pos) else 1
-
+            # print(f"  {i}", f"|| {s**e}-{a.val}={a.diff()} => pos={pos} ||", a)
             # need to check for loops
+            tried = set()
             while (a.val + pos_sign * pos ** a.e) in history:
+                tried.add(pos)
                 # if pos < a.s-2:  # -2 because 0 indexation and stop one before
-                if pos > 0:
+                if pos < a.s - 2:
                     pos += 1
                 else:
-                    pos = a.s-2 #0
+                    pos = a.s - 2  #0
                 pos_sign = -1 if a.is_set(pos) else 1
+                # reset to random when in loop
+                if pos in tried:
+                    a.set_random()
+                    pos = a.get_most_impactful_bit()
+                    pos_sign = -1 if a.is_set(pos) else 1
+
+            # if i % 523 == 0:
             history.add(a.val + pos_sign * pos ** a.e)
 
             a.flip_bit(pos)
         except:
             print("\titeration:", i)
-            print("\tdiff:", diff)
+            print("\tdiff:", a.diff())
             print("\tpos:", pos)
             print("\ta.val:", a.val)
             print("\tnew a.val:", (a.val + pos_sign * pos ** a.e))
             print("\ta.maxval:", a.maxval)
             print("\ta.vec:", a.vec.reverse())
             print("\ta:", a)
-            print(f"\t{i}", f"|| {s**e}-{a.val}={diff} => pos={pos} ||", a)
+            print(f"\t{i}", f"|| {s**e}-{a.val}={a.diff()} => pos={pos} ||", a)
             print("history:", end=" ")
-            pprint(history)
+            # pprint(history)
             raise
 
-        prev = diff
         diff = a.diff()
         if abs(diff) < abs(smallest) or diff == 0:
             print(f"found new smallest {diff}")
-            print(i, f"|| {s**e}-{a.val}={diff} => pos={pos} ||", a)
+            print(i, f"{(time() - start):.4f}s", f"|| {s**e}-{a.val}={diff} => pos={pos} ||", a)
             smallest = diff
             if diff == 0:
                 break
@@ -83,12 +86,15 @@ def greedy(s, e, rand="all"):
     timing = time() - start
     print(f"found smallest={smallest} after {i:e} iterations and {timing:.3f}s")
     print("*" * 40)
+    return i, timing, a
 
 if __name__ == "__main__":
-    # [3,5,6,15,12,25,40]
-    for e, s in enumerate([3,5,6,15,12], 1):  # s>=6 are too big
-        brute_force(s, e)
-
-    for e, s in enumerate([3,5,6,15], 1):
-        greedy(s, e, "all")
+    # [3,5,6,15,12,25,40,84,47,63,68,81],
+    # for e, s in enumerate([3,5,6,15,12], 1):  # s>=6 are too big
+    #     brute_force(s, e)
+    res = list()
+    for e, s in enumerate([3,5,6,15,12,25,40,84,47,63,68,81], 1):
+        res.append(greedy(s, e, "all"))
+    for r in res:
+        print(f"(n={r[2].e},s={r[2].s}) i={r[0]:10.2e},t={r[1]:.2f}s || {r[2]} ")
 
