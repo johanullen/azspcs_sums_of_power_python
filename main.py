50621e8488f5caf5355260ae23c1c95c55a5b4fa
Johan Ullen <johanullen@hotmail.com>
2020-04-04 16:16:08 +0200

    refactor: move algs to own files in algs folder

--- main.py
+++ main.py
@@ -1,94 +1,24 @@
 from time import time
-import random
-import math
-from pprint import pprint
 import traceback
 
-import utils
-from a import A
-
-
-def brute_force(s, e, rand="all"):
-    start = time()
-    print(f"starting exp={e} with s={s}")
-    a = A(s, e, rand)
-    smallest = diff = a.diff()
-    print(a.val, smallest, a)
-    for i in a.riter_values():
-        diff = a.diff()
-        if abs(diff) < abs(smallest) or diff == 0:
-            print(i, diff, a)
-            smallest = diff
-            if diff == 0:
-                break
-    timing = time() - start
-    print(f"found smallest={smallest} after {i:e} iterations and {timing:.3f}s")
-    print("*" * 40)
-
-
-def greedy(s, e, initial="all"):
-    start = time()
-    print(f"starting exp={e} with s={s}", flush=True)
-    a = A(s, e, initial)
-    smallest = a.diff()
-
-    history = {a.val}
-    i = 0
-    flip_or_rand = 0
-    while smallest != 0:
-        try:
-            pos = a.get_most_impactful_bit()
-            pos_sign = -1 if a.is_set(pos) else 1
-            # need to check for loops
-            tried = set()
-            if (a.val + pos_sign * pos ** a.e) in history:
-                flip_or_rand += 1
-                if flip_or_rand % 1:
-                    a.flip_all()
-                else:
-                    a.set_random()
-                    history = {a.val}
-                pos = a.get_most_impactful_bit()
-                pos_sign = -1 if a.is_set(pos) else 1
-
-            # if i % 2 == 0:
-            history.add(a.val + pos_sign * pos ** a.e)
-            a.flip_bit(pos)
-        except:
-            print("\titeration:", i)
-            print("\tdiff:", a.diff())
-            print("\tpos:", pos)
-            print("\ta.val:", a.val)
-            print("\tnew a.val:", (a.val + pos_sign * pos ** a.e))
-            print("\ta.maxval:", a.maxval)
-            print("\ta.vec:", a.vec.reverse())
-            print("\ta:", a)
-            print(f"\t{i}", f"|| {s**e}-{a.val}={a.diff()} => pos={pos} ||", a)
-            raise
-
-        diff = a.diff()
-        if abs(diff) < abs(smallest) or diff == 0:
-            # print(i, f"{(time() - start):.4f}s", f"|| {s**e}-{a.val}={diff} => pos={pos} ||", a, flush=True)
-            smallest = diff
-            if diff == 0:
-                break
-        i += 1
-
-    timing = time() - start
-    print(f"found smallest={smallest} after {i:10.2e} iterations and {timing:.3f}s", flush=True)
-    return i, timing, a
+from algs.brute_force import brute_force
+from algs.greedy_allow_overflow import greedy_allow_overflow
 
 if __name__ == "__main__":
     # [3,5,6,15,12,25,40,84,47,63,68,81,102,95,104],
-    # for e, s in enumerate([3,5,6,15,12], 1):  # s>=6 are too big
-    #     brute_force(s, e)
+    res = list()
+    for e, s in enumerate([3,5,6,15,12], 1):  # s>=6 are too big
+        res.append(brute_force(s, e))
+    for r in res:
+        print(f"(n={r[2].e},s={r[2].s}) i={r[0]:10.2e},t={r[1]:.2f}s || {r[2]} ")
+
     res = list()
     for e, s in enumerate([3,5,6,15,12,25,40,84,47,63,68,81,102,95,104], 1):
         # s > 70? is too big
         if s > 41:
             continue
         try:
-            res.append(greedy(s, e, "all"))
+            res.append(greedy_allow_overflow(s, e, "all"))
         except:
             traceback.print_exc()
             break
